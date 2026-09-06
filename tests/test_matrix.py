4604e5b21a162b00808460fbda0b23da0740c64b
Lasha <lashajinchar.adze16@gmail.com>
2026-09-06 10:05:53 +0400

    Test functions: validation, addition, mul, rmul

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -1,4 +1,13 @@
 from redpill.matrix import Matrix
+import pytest
+
+def test_validation():
+    with pytest.raises(ValueError, match="All rows must have the same number of columns."):
+        Matrix([
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8]
+        ])
 
 
 def test_addition():
@@ -17,4 +26,38 @@ def test_addition():
     assert result == Matrix([
         [6, 8],
         [10, 12]
+    ])
+
+def test_mul():
+    A = Matrix([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ])
+
+    scalar = 5
+
+    result = A * scalar
+
+    assert result == Matrix([
+        [5, 10, 15],
+        [20, 25, 30],
+        [35, 40, 45]
+    ])
+
+def test_rmul():
+    A = Matrix([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ])
+
+    scalar = 5
+
+    result = scalar * A
+
+    assert result == Matrix([
+        [5, 10, 15],
+        [20, 25, 30],
+        [35, 40, 45]
     ])
