528b267fc1bf92a3245cedc972a2d0eeaebf9642
Lasha <lashajinchar.adze16@gmail.com>
2026-09-09 22:37:02 +0400

    Matrix creation code in C

---
+++ csrc/matrix.c
@@ -0,0 +1,31 @@
+#include "matrix.h"
+#include <stdlib.h>
+
+Matrix *matrix_create(size_t rows, size_t columns)
+{
+    Matrix *matrix = malloc(sizeof(Matrix));
+
+    if (matrix == NULL)
+        return NULL;
+
+    matrix->rows = rows;
+    matrix->columns = columns;
+    matrix->data = malloc(rows * columns * sizeof(double));
+
+    if (matrix->data == NULL)
+    {
+        free(Matrix);
+        return NULL
+    }
+
+    return matrix
+
+    void matrix_free(Matrix *matrix)
+    {
+        if (matrix == NULL)
+            return;
+
+        free(matrix->data);
+        free(matrix);
+    }
+}
